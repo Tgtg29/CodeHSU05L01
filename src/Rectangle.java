b8c242527140111a7c533b36e56f8f3eaf9df8ab
Tgtg29 <tgtg7871@gmail.com>
2023-02-06 09:20:27 -0800

    Completed without toString method

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -7,13 +7,25 @@ public class Rectangle { //make public for CS A
     private double area;
     private double perimeter;
 
+
     //default constructor (make public for CS A)
+
+    /**
+     * Creates a rectangle with default parameters
+     */
     public Rectangle(){
         width = 1.0;
         height = 1.0;
     }
 
     //overloaded constructor (with parameters)
+
+    /**
+     * Creates a Rectangle object with specified width and height
+     *
+     * @param w width of rectangle
+     * @param h height of rectangle
+     */
     public Rectangle(double w, double h){
         width = w;
         height = h;
@@ -23,20 +35,64 @@ public class Rectangle { //make public for CS A
         this.width = width;
     }
 
-
-
     //accessors
 
+    /**
+     *
+     * @return the width of the rectangle
+     */
+    public double getWidth(){
+        return width;
+    }
 
+    public double getHeight(){
+        return height;
+    }
+
+    public double getArea(){
+        return area;
+    }
+
+    public double getPerimeter(){
+        return perimeter;
+    }
 
     //mutators
+
+    /**
+     * Allows the user to change the width of the rectangle
+     *
+     * @param newWidth desired width
+     */
+    public void setWidth(double newWidth){
+        width = newWidth;
+    }
+
+    /**
+     * Modifies the height of the rectangle
+     * @param newHeight
+     */
+    public void setHeight(double newHeight){
+        height = newHeight;
+    }
 
 
     //other methods
 
+    /**
+     * Calculates area of rectangle
+     * @return
+     */
+    public void calculateArea(){
+        area = height*width;
+    }
 
+    public void calculatePerimeter(){
+        perimeter = 2*height + 2*width;
+    }
 
 
     //toString method
+
 
 }
--- src/RectangleTester.java
+++ src/RectangleTester.java
@@ -1,6 +1,11 @@
 public class RectangleTester {
     public static void main(String[] args) {
         //test your rectangle here
+        Rectangle rec1 = new Rectangle(7.45676, 394.488543);
+        double expectedPerimeter = 2*7.45676 + 2*394.488543;
+        rec1.calculatePerimeter();
+        System.out.println(rec1.getPerimeter() == expectedPerimeter);
+        System.out.println(rec1.getPerimeter());
 
     }
 }
